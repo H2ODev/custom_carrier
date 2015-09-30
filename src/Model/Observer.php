c1ccecdc06e44cdb4dc80070d1f71be399d0da9d
Marvin Oßwald <mo@h2om.de>
2015-09-30 18:06:35 +0200

    init

---
+++ src/Model/Observer.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: marvinosswald
+ * Date: 25.09.15
+ * Time: 11:18.
+ */
+class H2ODev_CustomCarrier_Model_Observer
+{
+    public function __construct()
+    {
+    }
+}
